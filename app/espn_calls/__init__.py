9790966e2fa44042bed2c6677fa35d800c4ebaeb
anaytakk <anay.takkallapalli@gmail.com>
2026-02-02 11:45:19 -0800

    add new function calls for schedule, player rankings, trade value

--- app/espn_calls/__init__.py
+++ app/espn_calls/__init__.py
@@ -1,5 +1,8 @@
 from .boom_bust_calls import * 
 from .waiver_injury_calls import * 
+from .rankings_calls import *
+from .schedule_calls import *
+from .trade_calls import *
 
 __all__ = [
     "compute_boom_score",
@@ -7,4 +10,10 @@ __all__ = [
     "get_roster", 
     "get_injured_players",
     "get_free_agents"
+    "get_all_players",
+    "generate_player_rankings",
+    "get_team_schedule",
+    "get_games_left_in_week",
+    "compute_player_trade_value",
+    "evaluate_trade",
 ]
