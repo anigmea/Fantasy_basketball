94228ad9c6ca07c84c70933d604c9e2b657fb4db
anaytakk <anay.takkallapalli@gmail.com>
2026-02-02 12:25:46 -0800

    Initial commit

--- app/espn_calls/__init__.py
+++ app/espn_calls/__init__.py
@@ -1,10 +1,19 @@
 from .boom_bust_calls import * 
 from .waiver_injury_calls import * 
+from .rankings_calls import *
+from .schedule_calls import *
+from .trade_calls import *
 
 __all__ = [
     "compute_boom_score",
     "get_boom_bust_players",
     "get_roster", 
     "get_injured_players",
-    "get_free_agents"
+    "get_free_agents",
+    "get_all_players",
+    "generate_player_rankings",
+    "get_team_schedule",
+    "get_games_left_in_week",
+    "compute_player_trade_value",
+    "evaluate_trade",
 ]
--- db_throwaway_code_espn.py
+++ db_throwaway_code_espn.py
@@ -6,6 +6,20 @@ from app.espn_calls import *
 league = League(league_id = app.config["LEAGUE_ID"], year = 2026, swid = app.config["SWID"], espn_s2 = app.config["ESPN_S2"])
 print(get_boom_bust_players(league))
 
+# Example usages of new helpers
+try:
+    print("Top 10 players by avg_points:")
+    print(generate_player_rankings(league, by='avg_points', top_n=10))
+except Exception as e:
+    print("Error generating rankings:", e)
+
+try:
+    print("Games left in week 1:")
+    print(get_games_left_in_week(league, week=1))
+except Exception as e:
+    print("Error getting schedule:", e)
+
+
 
 
 
