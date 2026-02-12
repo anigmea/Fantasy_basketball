8a5b9faaf4b2c009c12eddabd232487d44391491
Nathan Nguyen <nvnguyen109@gmail.com>
2026-02-12 15:39:31 -0800

    finished v1 model for injury_waiver

--- app/espn_calls/waiver_injury_calls.py
+++ app/espn_calls/waiver_injury_calls.py
@@ -1,5 +1,4 @@
-from typing import Dict, List, Optional
-from firebase_admin import firestore
+from typing import Optional
 
 # Collections
 TEAM_PLAYER_COL = "team_players"
@@ -8,12 +7,6 @@ FREE_AGENTS_COL = "free_agents"
 def get_team_players(db):
     '''Returns all rostered players across the League'''
     q = db.collection(TEAM_PLAYER_COL)
-    return [doc.to_dict() for doc in q.stream()]
-
-
-def get_injured_players(db):
-    '''Returns injured rostered players across the League'''
-    q = db.collection(TEAM_PLAYER_COL).where("injured", "==", True)
     return [doc.to_dict() for doc in q.stream()]
 
 
@@ -30,8 +23,8 @@ def get_free_agents(db, position: Optional[str] = None, injured: Optional[bool] 
     
     return [doc.to_dict() for doc in q.stream()]
 
-    __all__ = [
-        "get_team_players",
-        "get_injured_players",
-        "get_free_agents"
-    ]
+
+__all__ = [
+    "get_team_players",
+    "get_free_agents"
+]
